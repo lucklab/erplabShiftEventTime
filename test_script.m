e94b3af7374cff89e2ce2fc20ab14c8009a29f58
jasonarita <jtkarita-admin@percpt.local>
2015-10-05 15:54:29 -0700

    minor cleaning to test script

--- test_script.m
+++ test_script.m
@@ -1,5 +1,5 @@
 %% Test script
-function [outputEEG, eventtable_output] = test_script()
+function [outputEEG] = test_script()
 try
     
     eeglab;
@@ -34,40 +34,3 @@ catch err;
 end
 
 end % function
-
-%% Function
-
-function outputEEG = erplabEegTimeShift(inputEEG, eventcodes, timeshift)
-
-outputEEG = inputEEG;
-
-sample_shift = timeshift * inputEEG.srate;
-
-% Convert EEG.data structure to a Matlab table
-eventsTable      = struct2table(inputEEG.event);
-
-% Convert event codes to a categorical variable type 
-eventsTable.type = categorical(eventsTable.type);
-eventcodes       = categorical(eventcodes);
-
-% Copy the original latencies
-% old_latency = eventsTable.latency;
-% eventsTable = [eventsTable table(old_latency)];
-
-
-rows                   = ismember(eventsTable.type, eventcodes);
-vars                   = {'latency'};
-eventsTable{rows,vars} = eventsTable{rows,vars}+sample_shift;
-
-
-% check for latency differences
-% diff_latency = eventsTable.latency - eventsTable.old_latency;
-% eventsTable = [eventsTable table(diff_latency)];
-
-eventsTable.type = char(eventsTable.type);
-outputEEG.event  = table2struct(eventsTable)';
-
-% check for out of bound events / Re-sort ur events
-outputEEG = eeg_checkset(outputEEG, 'eventconsistency', 'checkur'); 
-
-end
